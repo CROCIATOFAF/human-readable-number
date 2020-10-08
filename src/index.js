544f1790660705d6402faf22e5c6a623d352930c
CROCIATOFAF <48692205+CROCIATOFAF@users.noreply.github.com>
2020-10-08 23:56:49 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,13 @@
 module.exports = function toReadable (number) {
-  
+    let zeroToNineteen = ["zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten",
+    "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen"];
+    let twentyToNinety = ["twenty", "thirty", "forty", "fifty", "sixty", "seventy", "eighty", "ninety"];
+
+    if (number < 20) {
+        return zeroToNineteen[number];
+    } else if (number < 100) {
+        return twentyToNinety[Math.floor(number / 10) - 2] + (number % 10 !== 0 ? " " + toReadable(number % 10) : "");
+    } else if (number < 1000) {
+        return zeroToNineteen[Math.floor(number / 100)] + " hundred" + (number % 100 !== 0 ? " " + toReadable(number % 100) : "");
+    }
 }
